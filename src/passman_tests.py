07f58ea6422d27bc135f8984ec95b28d1d1cb2c9
Maëlick Claes <himself@maelick.net>
2012-01-06 03:00:38 +0100

    License correction

--- src/passman_tests.py
+++ src/passman_tests.py
@@ -2,20 +2,20 @@
 #
 # Copyright 2011 Maëlick Claes <himself@maelick.net>
 #
-# This file is part of PassML.
+# This file is part of PassMAN.
 #
-# PassML is free software: you can redistribute it and/or modify
+# PassMAN is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# PassML is distributed in the hope that it will be useful,
+# PassMAN is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with PassML.  If not, see <http://www.gnu.org/licenses/>.
+# along with PassMAN.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest, random, os, copy
 from passgen import PassmanGenerator, GeneratorManager
